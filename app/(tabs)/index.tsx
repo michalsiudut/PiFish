f235050a137a0d0d3bd81eb963c1b42a5ec79589
michalsiudut <michal.siudut.ms@gmail.com>
2025-11-04 14:34:57 +0100

    top bard created with profile icon and bell icon

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
+import { icons as images } from '../../constants/icons';
 
 
 export default function Index() {
@@ -6,8 +7,22 @@ export default function Index() {
   const user = "Michał";
   return (
     <>
-      <View className="mt-20 ml-4">
-        <Text className="text-4xl text-primary font-bold">Hello, {user}</Text>
+      <View className="mt-20 ml-4 flex-row items-center justify-between pr-4">
+        <Text className="text-4xl text-primary font-bold">Hello, {user}!</Text>
+        <View className="justify-center flex-row items-center">
+          <Image source={images.Bell} style={{
+            width: 30,
+            height: 30,
+          }}
+            className="mr-4">
+          </Image>
+          <Image source={images.ProfileIcon} style={{
+            width: 40,
+            height: 40,
+          }}>
+          </Image>
+        </View>
+
       </View >
     </>
   );
--- constants/icons.ts
+++ constants/icons.ts
@@ -2,10 +2,15 @@ import Calendar from "../assets/icons/Calendar.png"
 import Home from "../assets/icons/Home.png"
 import Profile from "../assets/icons/Profile.png"
 import Settings from "../assets/icons/Settings.png"
+import Bell from "../assets/icons/Bell.png"
+import ProfileIcon from "../assets/icons/ProfileIcon.png"
+
 
 export const icons = {
     Home,
     Profile,
     Calendar,
     Settings,
+    Bell,
+    ProfileIcon,
 }

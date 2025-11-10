3899be868d1cee111650aeffb8bdd23ecc28b879
michalsiudut <michal.siudut.ms@gmail.com>
2025-11-10 17:48:17 +0100

    Adding icons to ValueInput and scroll View repaired

--- app/(auth)/SignUpPage.tsx
+++ app/(auth)/SignUpPage.tsx
@@ -44,44 +44,43 @@ export default function SignUpPage() {
     return (
 
         <SafeAreaView style={{ flex: 1 }}>
-            <View className='justify-center items-center mt-12'>
-                <Image source={icons.WelcomeIcon} style={styles.icon}></Image>
-            </View>
-            <View className='justify-center items-center mt-7'>
-                <Text style={styles.text} className='text-4xl'>Stwórz konto</Text>
-            </View>
             <ScrollView style={{ flex: 1 }}>
+                <View className='justify-center items-center mt-12'>
+                    <Image source={icons.WelcomeIcon} style={styles.icon}></Image>
+                </View>
+                <View className='justify-center items-center mt-7'>
+                    <Text style={styles.text} className='text-4xl'>Stwórz konto</Text>
+                </View>
                 <View className='mt-6'>
-                    <ValueInput title='Email' placeholder='Wpisz swój email' color='#61897F'></ValueInput>
+                    <ValueInput title='Email' placeholder='Wpisz swój email' color='#61897F' iconName='Mail'></ValueInput>
                 </View>
                 <View className='mt-2'>
-                    <ValueInput title='Nazwa użytkownika' placeholder='Wpisz swoją nazwę użytkownika' color='#61897F'></ValueInput>
+                    <ValueInput title='Nazwa użytkownika' placeholder='Wpisz swoją nazwę użytkownika' color='#61897F' iconName='Name'></ValueInput>
                 </View>
                 <View className='mt-2'>
-                    <ValueInput title='Imie' placeholder='Wpisz swoje imię' color='#61897F'></ValueInput>
+                    <ValueInput title='Imie' placeholder='Wpisz swoje imię' color='#61897F' iconName='Name'></ValueInput>
                 </View>
                 <View className='mt-2'>
-                    <ValueInput title='Nazwisko' placeholder='Wpisz swoje nazwisko' color='#61897F'></ValueInput>
+                    <ValueInput title='Nazwisko' placeholder='Wpisz swoje nazwisko' color='#61897F' iconName='Name'></ValueInput>
                 </View>
                 <View className='mt-2'>
-                    <ValueInput title='Miasto' placeholder='Wpisz swoje miasto' color='#61897F'></ValueInput>
+                    <ValueInput title='Miasto' placeholder='Wpisz swoje miasto' color='#61897F' iconName='City'></ValueInput>
                 </View>
                 <View className='mt-2'>
-                    <ValueInput title='Hasło' placeholder='Wpisz swoje hasło' color='#61897F' secureTextEntry={true}></ValueInput>
+                    <ValueInput title='Hasło' placeholder='Wpisz swoje hasło' color='#61897F' secureTextEntry={true} iconName='Lock'></ValueInput>
                 </View>
                 <View className='mt-2'>
-                    <ValueInput title='Potwierdź hasło' placeholder='Wpisz swoje hasło jeszcze raz' color='#61897F' secureTextEntry={true}></ValueInput>
+                    <ValueInput title='Potwierdź hasło' placeholder='Wpisz swoje hasło jeszcze raz' color='#61897F' secureTextEntry={true} iconName='Key'></ValueInput>
                 </View>
                 <View style={{ height: 30 }}></View>
+                <ButtonFunction text='Zarejestruj się' onChange={SignUp} textColor='primary' />
+                <View className='mb-4 justify-center items-center flex-row gap-1'>
+                    <Text style={styles.textShadow}>
+                        Nie masz konta?
+                    </Text>
+                    <TouchableText text='Zaloguj się' onChange={switchToSignIn} fontSize={13} color='#14b8a6'></TouchableText>
+                </View>
             </ScrollView>
-            <ButtonFunction text='Zarejestruj się' onChange={SignUp} textColor='primary' />
-            <View className='mb-4 justify-center items-center flex-row gap-1'>
-                <Text style={styles.textShadow}>
-                    Nie masz konta?
-                </Text>
-                <TouchableText text='Zaloguj się' onChange={switchToSignIn} fontSize={13} color='#14b8a6'></TouchableText>
-            </View>
-
         </SafeAreaView>
     )
 }

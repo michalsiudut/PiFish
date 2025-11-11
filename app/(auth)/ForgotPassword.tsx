8e2407588c8a256317b89def7f8713dc51d1a9b2
michalsiudut <michal.siudut.ms@gmail.com>
2025-11-11 21:22:47 +0100

    simple bux fix with email sended

--- app/(auth)/ForgotPassword.tsx
+++ app/(auth)/ForgotPassword.tsx
@@ -1,10 +1,10 @@
 import { ButtonFunction } from '@/app/components/buttons/ButtonFunction';
 import { icons } from '@/constants/icons';
-import React, { useEffect, useState } from 'react';
-import { Keyboard, StyleSheet, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
-
+import { auth } from '@/FirebaseConfig';
+import { sendPasswordResetEmail } from '@firebase/auth';
 import { useRouter } from 'expo-router';
-import { ActivityIndicator, Image, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, Image, Keyboard, StyleSheet, Text, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import ValueInput from '../components/ValueInput';
 import { useFontStatus } from '../hooks/useFontStatus';
@@ -33,63 +33,62 @@ export default function ForgotPassword() {
     }
 
     const sendCode = async () => {
-        console.log(email);
-        console.log("success");
         try {
-            // TODO logic to send the code 
+            await sendPasswordResetEmail(auth, email);
+            alert("Jeśli adres e-mail jest zarejestrowany, link do resetowania hasła został wysłany.");
 
         } catch (error) {
-            console.error(error);
+            // handle error
         }
-    }
 
-    return (
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <SafeAreaView>
-                <TouchableOpacity onPress={goBack}>
-                    <Image source={icons.Arrow} style={styles.iconArrow}></Image>
-                </TouchableOpacity>
-                <View className='mt-16 justify-center items-center'>
-                    <Image source={icons.WelcomeIcon} style={styles.icon}></Image>
-                </View>
-                <View className='mt-6 justify-center items-center'>
-                    <Text style={styles.text} className='text-4xl'>
-                        Odzyskaj hasło
-                    </Text>
-                    <View className='mt-4 justify-center'>
-                        <Text style={styles.textShadow} className='text-center'>
-                            Podaj swój adre e-mail, aby otrzymać kod weryfikacyjny.
-                        </Text>
+        return (
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <SafeAreaView>
+                    <TouchableOpacity onPress={goBack}>
+                        <Image source={icons.Arrow} style={styles.iconArrow}></Image>
+                    </TouchableOpacity>
+                    <View className='mt-16 justify-center items-center'>
+                        <Image source={icons.WelcomeIcon} style={styles.icon}></Image>
                     </View>
-                </View>
-                <View className='mt-8'>
-                    <ValueInput title='Email' placeholder='Wpisz swój adres e-mail' color='#61897F' value={email} onChangeText={setEmail}></ValueInput>
-                </View>
-                <ButtonFunction text='Wyślij kod' onChange={sendCode} textColor='primary'>
-                </ButtonFunction>
-            </SafeAreaView >
-        </TouchableWithoutFeedback >
-    )
-}
-
-const styles = StyleSheet.create({
-
-    iconArrow: {
-        width: 32,
-        height: 32,
-        marginLeft: 16,
-    },
-    icon: {
-        width: 53,
-        height: 53,
-        tintColor: "#14b8a6"
-    },
-    text: {
-        fontFamily: 'Lexend-Bold',
-        color: 'primary',
-    },
-    textShadow: {
-        fontFamily: 'Lexend-Bold',
-        color: '#61897F',
+                    <View className='mt-6 justify-center items-center'>
+                        <Text style={styles.text} className='text-4xl'>
+                            Odzyskaj hasło
+                        </Text>
+                        <View className='mt-4 justify-center'>
+                            <Text style={styles.textShadow} className='text-center'>
+                                Podaj swój adre e-mail, aby otrzymać kod weryfikacyjny.
+                            </Text>
+                        </View>
+                    </View>
+                    <View className='mt-8'>
+                        <ValueInput title='Email' placeholder='Wpisz swój adres e-mail' color='#61897F' value={email} onChangeText={setEmail} iconName='Mail'></ValueInput>
+                    </View>
+                    <ButtonFunction text='Wyślij kod' onChange={sendCode} textColor='primary'>
+                    </ButtonFunction>
+                </SafeAreaView >
+            </TouchableWithoutFeedback >
+        )
     }
-})
+
+    const styles = StyleSheet.create({
+
+        iconArrow: {
+            width: 32,
+            height: 32,
+            marginLeft: 16,
+        },
+        icon: {
+            width: 53,
+            height: 53,
+            tintColor: "#14b8a6"
+        },
+        text: {
+            fontFamily: 'Lexend-Bold',
+            color: 'primary',
+        },
+        textShadow: {
+            fontFamily: 'Lexend-Bold',
+            color: '#61897F',
+        }
+    })
+}

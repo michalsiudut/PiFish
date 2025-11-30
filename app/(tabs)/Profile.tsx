12f7c5bec640374751c80095282faa0e6bae3880
michalsiudut <michal.siudut.ms@gmail.com>
2025-11-30 11:58:33 +0100

    Refactor flatlist to simple .map to add scrollview to whole profile screen

--- app/(tabs)/Profile.tsx
+++ app/(tabs)/Profile.tsx
@@ -1,4 +1,5 @@
-import { DEGREES_DATA } from '@/constants/degrees'
+import { DegreeListItem } from "@/app/components/DegreeListItem"
+import { DEGREES_DATA } from "@/constants/degrees"
 import { icons as images } from "@/constants/icons"
 import { auth } from '@/services/FirebaseConfig'
 import { supabase } from '@/services/supabase'
@@ -6,9 +7,8 @@ import { updateUserSingleData } from '@/services/user_services/updateUserSingleD
 import * as ImagePicker from "expo-image-picker"
 import { useRouter } from 'expo-router'
 import React, { useEffect, useState } from 'react'
-import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { DegreeListItem } from '../components/DegreeListItem'
 import Loader from '../components/Loader'
 import { useUser } from '../context/UserContext'
 import { caluclateDegree } from '../hooks/caluclateDegree'
@@ -24,9 +24,6 @@ export default function Profile() {
     const [percentage, setPercentage] = useState(0);
     const [isLoading, setIsLoading] = useState(false);
     const [isNameEmpty, setIsNameEmpty] = useState(true);
-
-    const [isToastVisible, setIsToastVisible] = useState(false);
-    const [toastMessage, setToastMessage] = useState("");
 
     useEffect(() => {
         if (name != "Imię") {
@@ -123,52 +120,57 @@ export default function Profile() {
                 </View>
             </View>
             {isLoading == true ? <Loader zIndex={0}></Loader> : <View></View>}
-            <View className='mt-10 justify-center items-center'>
-                <View className='relative'>
-                    {profilePhoto == "" ? <Image source={images.Default} style={styles.image} className='border-4 rounded-full border-secondary '></Image> : <Image source={{ uri: profilePhoto }} style={styles.image} className='border-4 rounded-full border-secondary '></Image>}
-                    <TouchableOpacity onPress={() => handleEditPhotoPress()} className='absolute bottom-0 right-0 bg-white p-2 rounded-full border-4 border-secondary'>
-                        <Image source={images.Edit} style={styles.edit} />
-                    </TouchableOpacity>
+            <ScrollView>
+                <View className='mt-10 justify-center items-center'>
+                    <View className='relative'>
+                        {profilePhoto == "" ? <Image source={images.Default} style={styles.image} className='border-4 rounded-full border-secondary '></Image> : <Image source={{ uri: profilePhoto }} style={styles.image} className='border-4 rounded-full border-secondary '></Image>}
+                        <TouchableOpacity onPress={() => handleEditPhotoPress()} className='absolute bottom-0 right-0 bg-white p-2 rounded-full border-4 border-secondary'>
+                            <Image source={images.Edit} style={styles.edit} />
+                        </TouchableOpacity>
+                    </View>
                 </View>
-            </View>
-            <View className='flex justify-center items-center mt-4 relative'>
-                {name == "" ? <Text style={[styles.text, {
-                    fontSize: 25
-                }]}>Imię Nazwisko</Text> : <Text style={[styles.text, {
-                    fontSize: 25
-                }]}>{name} {surname}</Text>}
-            </View>
-            <View className='flex justify-center items-center mt-2'>
-                <Text style={[styles.textShadow, { fontSize: 13 }]}>{nick}</Text>
-            </View>
-            <View className='flex justify-center items-center mt-2'>
-                <Text style={[styles.textShadow, { fontSize: 13 }]}>{email}</Text>
-            </View>
-
-            <View className='flex-row mt-4 ml-5 justify-between items-center'>
-                <Text style={[styles.text, { fontSize: 20 }]}>Postęp</Text>
-                <Text style={[styles.text, { color: '#14b8a6' }]} className='mr-4'>{xp} XP</Text>
-            </View>
-            <View className='flex-row justify-between mr-4 mt-4 items-center'>
-                <View className='flex-row ml-4'>
-                    <Text style={styles.textShadow}> Poziom: </Text>
-                    <Text style={[styles.textShadow, { color: "#14b8a6" }]}> {degree} </Text>
+                <View className='flex justify-center items-center mt-4 relative'>
+                    {name == "" ? <Text style={[styles.text, {
+                        fontSize: 25
+                    }]}>Imię Nazwisko</Text> : <Text style={[styles.text, {
+                        fontSize: 25
+                    }]}>{name} {surname}</Text>}
                 </View>
-                <Text style={styles.textShadow}> {xp}/{maxXP} </Text>
-            </View>
+                <View className='flex justify-center items-center mt-2'>
+                    <Text style={[styles.textShadow, { fontSize: 13 }]}>{nick}</Text>
+                </View>
+                <View className='flex justify-center items-center mt-2'>
+                    <Text style={[styles.textShadow, { fontSize: 13 }]}>{email}</Text>
+                </View>
 
-            <View className='w-auto bg-slate-300 h-4 ml-5 mt-2 rounded-xl mr-5'>
-                <View className='bg-secondary h-4 flex rounded-xl' style={{ width: `${percentage}%` }}></View>
-            </View>
-            <View className='flex-row ml-5 mt-2 justify-end mr-4'>
-                <Text style={styles.textShadow}>Następny poziom: </Text>
-                <Text style={[styles.textShadow, { color: '#14b8a6' }]}>{nextDegree}</Text>
-            </View>
-            <View className='ml-5'>
-                <Text style={[styles.text, { fontSize: 20 }]}>Skala poziomów</Text>
-            </View>
-            <FlatList className="mt-4 ml-5 mr-4" data={DEGREES_DATA} renderItem={({ item }) => (<DegreeListItem item={item} currentXp={xp} />)}>
-            </FlatList>
+                <View className='flex-row mt-4 ml-5 justify-between items-center'>
+                    <Text style={[styles.text, { fontSize: 20 }]}>Postęp</Text>
+                    <Text style={[styles.text, { color: '#14b8a6' }]} className='mr-4'>{xp} XP</Text>
+                </View>
+                <View className='flex-row justify-between mr-4 mt-4 items-center'>
+                    <View className='flex-row ml-4'>
+                        <Text style={styles.textShadow}> Poziom: </Text>
+                        <Text style={[styles.textShadow, { color: "#14b8a6" }]}> {degree} </Text>
+                    </View>
+                    <Text style={styles.textShadow}> {xp}/{maxXP} </Text>
+                </View>
+
+                <View className='w-auto bg-slate-300 h-4 ml-5 mt-2 rounded-xl mr-5'>
+                    <View className='bg-secondary h-4 flex rounded-xl' style={{ width: `${percentage}%` }}></View>
+                </View>
+                <View className='flex-row ml-5 mt-2 justify-end mr-4'>
+                    <Text style={styles.textShadow}>Następny poziom: </Text>
+                    <Text style={[styles.textShadow, { color: '#14b8a6' }]}>{nextDegree}</Text>
+                </View>
+                <View className='ml-5'>
+                    <Text style={[styles.text, { fontSize: 20 }]}>Skala poziomów</Text>
+                </View>
+                <View className="mt-3">
+                    {DEGREES_DATA.map((item) => (
+                        <DegreeListItem key={item.id} item={item} currentXp={xp}></DegreeListItem>
+                    ))}
+                </View>
+            </ScrollView>
 
         </SafeAreaView >
     )
--- app/components/DegreeListItem.js
+++ app/components/DegreeListItem.js
@@ -3,7 +3,6 @@ import { StyleSheet, Text, View } from "react-native";
 
 export const DegreeListItem = ({ item, currentXp }) => {
 
-    // Sprawdza, czy aktualny XP kwalifikuje się do tego stopnia
     const isActive = currentXp >= item.minXp && currentXp < item.maxXp;
     const isDone = currentXp < item.maxXp;
 
@@ -13,16 +12,18 @@ export const DegreeListItem = ({ item, currentXp }) => {
 
     return (
         <>
-            {isDone == false ?
-                ((<View className="flex-row rounded-xl justify-between h-14 border-2 border-yellow-400 bg-yellow-100 mb-4 items-center" >
-                    <Text className="ml-4" style={styles.text}>{item.name}</Text>
-                    <Text className="mr-4" style={styles.text}>{progressText}</Text>
-                </View >))
-                :
-                (<View className={isActive ? "flex-row rounded-xl justify-between h-14 border-2 border-secondary mb-4 items-center" : "flex-row rounded-xl justify-between h-14 bg-slate-200 mb-4 items-center"}>
-                    <Text className="ml-4" style={styles.text}>{item.name}</Text>
-                    <Text className="mr-4" style={styles.text}>{progressText}</Text>
-                </View >)}
+            <View className="mr-4 ml-4">
+                {isDone == false ?
+                    ((<View className="flex-row rounded-xl justify-between h-14 border-2 border-yellow-400 bg-yellow-100 mb-4 items-center" >
+                        <Text className="ml-4" style={styles.text}>{item.name}</Text>
+                        <Text className="mr-4" style={styles.text}>{progressText}</Text>
+                    </View >))
+                    :
+                    (<View className={isActive ? "flex-row rounded-xl justify-between h-14 border-2 border-secondary mb-4 items-center" : "flex-row rounded-xl justify-between h-14 bg-slate-200 mb-4 items-center"}>
+                        <Text className="ml-4" style={styles.text}>{item.name}</Text>
+                        <Text className="mr-4" style={styles.text}>{progressText}</Text>
+                    </View >)}
+            </View>
         </>
     );
 };

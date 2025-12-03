6004b83a6cb3838a756b4b33a70b579731da21e5
michalsiudut <michal.siudut.ms@gmail.com>
2025-12-03 19:21:12 +0100

    adding EditProfileComponent and styling impoved on EditProfile

--- app/(pages)/EditProfile.tsx
+++ app/(pages)/EditProfile.tsx
@@ -7,9 +7,11 @@ import * as ImagePicker from "expo-image-picker"
 import { useRouter } from 'expo-router'
 import { goBack } from "expo-router/build/global-state/routing"
 import { useEffect, useState } from 'react'
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Image, StyleSheet, Text, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useUser } from '../context/UserContext'
+import EditProfileComponent from "../components/EditProfileComponent"
+import { Keyboard } from "react-native"
 
 export default function EditProfile() {
 
@@ -90,23 +92,27 @@ export default function EditProfile() {
 
 
     return (
-        <SafeAreaView>
-            <View className='justify-between items-center flex-row'>
-                <TouchableOpacity onPress={goBack}>
-                    <Image style={styles.icons} source={images.Arrow} className="ml-4"></Image>
-                </TouchableOpacity>
-                <Text style={styles.text}>Personal info</Text>
-                <View className="mr-[35px]"></View>
-            </View>
-            <View className='mt-10 justify-center items-center'>
-                <View className='relative'>
-                    {localPhoto == "" ? <Image source={images.Default} style={styles.image} className='border-4 rounded-full border-secondary '></Image> : <Image source={{ uri: localPhoto }} style={styles.image} className='border-4 rounded-full border-secondary '></Image>}
-                    <TouchableOpacity onPress={() => handleEditPhotoPress()} className='absolute bottom-0 right-0 bg-white p-2 rounded-full border-4 border-secondary'>
-                        <Image source={images.Edit} style={styles.edit} />
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <SafeAreaView>
+                <View className='justify-between items-center flex-row'>
+                    <TouchableOpacity onPress={goBack}>
+                        <Image style={styles.icons} source={images.Arrow} className="ml-4"></Image>
                     </TouchableOpacity>
+                    <Text style={styles.text}>Personal info</Text>
+                    <View className="mr-[35px]"></View>
                 </View>
-            </View>
-        </SafeAreaView>
+                <View className='mt-10 justify-center items-center'>
+                    <View className='relative'>
+                        {localPhoto == "" ? <Image source={images.Default} style={styles.image} className='border-4 rounded-full border-secondary '></Image> : <Image source={{ uri: localPhoto }} style={styles.image} className='border-4 rounded-full border-secondary '></Image>}
+                        <TouchableOpacity onPress={() => handleEditPhotoPress()} className='absolute bottom-0 right-0 bg-white p-2 rounded-full border-4 border-secondary'>
+                            <Image source={images.Edit} style={styles.edit} />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <View className="mt-6"></View>
+                <EditProfileComponent title="Imie" placeholderValue="Wprowadź swoje imie" />
+            </SafeAreaView>
+        </TouchableWithoutFeedback>
     )
 }
 
